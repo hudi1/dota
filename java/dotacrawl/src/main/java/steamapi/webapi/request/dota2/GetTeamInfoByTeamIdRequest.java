8ec16cb0cbfd15ee67009675bbe9ea59d072ba4c
Tomas Hudec <hudec.tomas1@gmail.com>
2021-01-04 00:34:58 +0100

    FIX/ADD json

--- java/dotacrawl/src/main/java/steamapi/webapi/request/dota2/GetTeamInfoByTeamIdRequest.java
+++ java/dotacrawl/src/main/java/steamapi/webapi/request/dota2/GetTeamInfoByTeamIdRequest.java
@@ -15,7 +15,7 @@ public class GetTeamInfoByTeamIdRequest extends SteamWebApiRequest{
 
         private Integer teamsRequested;
 
-        public static final String REQUEST_PARAM_START_AT_TEAM_ID = "start_at_team_id ";
+        public static final String REQUEST_PARAM_START_AT_TEAM_ID = "start_at_team_id";
 
         public static final String REQUEST_PARAM_TEAMS_REQUESTED = "teams_requested";
 
